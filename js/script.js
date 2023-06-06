d23fe7ba2e8dfae52286e3fb21a3a4d4d6274710
SearchSerpent <107382632+leiocopio@users.noreply.github.com>
2023-06-06 18:06:38 +0800

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Get references to HTML elements
+const taskInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
+
+// Add click event listener to the "Add Task" button
+addButton.addEventListener('click', addTask);
+
+function addTask() {
+    const taskText = taskInput.value;
+
+    if (taskText) {
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('tasks');
+        taskItem.textContent = taskText;
+
+        const deleteButton = document.createElement('button'); // Create a delete button for each task
+        deleteButton.textContent = 'Delete'; // Set the text content of the delete button
+        deleteButton.addEventListener('click', deleteTask); // Add click event listener to the delete button
+
+        taskItem.appendChild(deleteButton); // Append the delete button to the task item
+        taskList.appendChild(taskItem);
+        taskInput.value = '';
+    }
+}
+
+function deleteTask(event) {
+    const taskItem = event.target.parentElement; // Get the parent element (task item) of the delete button
+    taskList.removeChild(taskItem); // Remove the task item from the task list
+}
